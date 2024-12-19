a10515e265eefe6c3ab6331775e6955997572338
有点小激动 <3030036858@qq.com>
2024-12-19 11:57:26 +0800

    first commit

---
+++ oj-modules/oj-friend/src/main/java/com/excited/friend/OjFriendsApplication.java
@@ -0,0 +1,11 @@
+package com.excited.friend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjFriendsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OjFriendsApplication.class, args);
+    }
+}
